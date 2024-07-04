051d78e724f5afc222c3ce772b817fd3dc24ab33
Dana <Dana.ackerman0914@gmail.com>
2024-07-04 20:23:07 +0500

    Added pojo entity classes and hibernate annotations

--- src/main/java/model/entities/User.java
+++ src/main/java/model/entities/User.java
@@ -1,67 +1,27 @@
 package model.entities;
 
-import java.io.Serializable;
+import jakarta.persistence.*;
+import lombok.*;
+
+
 import java.time.LocalDate;
-import java.util.Objects;
+import java.util.List;
 
-public class User implements Serializable {
-    private long id;
+@Getter
+@Setter
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String username;
+    @Column(name = "creation_date")
     private LocalDate creationDate;
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user")
+    private List<Ticket> tickets;
 
-    private Ticket ticket;
-    public User() {
-
-    }
-    public User(String username, LocalDate creationDate) {
-        this.username = username;
-        this.creationDate = creationDate;
-    }
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public LocalDate getCreationDate() {
-        return creationDate;
-    }
-    public void setCreationDate(LocalDate creationDate) {
-        this.creationDate = creationDate;
-    }
-    public Ticket getTicket() {
-        return ticket;
-    }
-    public void setTicket(Ticket ticket) {
-        this.ticket = ticket;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id == user.id && Objects.equals(username, user.username) && Objects.equals(creationDate, user.creationDate) && Objects.equals(ticket, user.ticket);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, username, creationDate, ticket);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", creationDate=" + creationDate +
-                ", ticket=" + ticket +
-                '}';
-    }
 }
