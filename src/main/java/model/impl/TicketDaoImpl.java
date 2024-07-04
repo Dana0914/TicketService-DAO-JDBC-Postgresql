2cb9b64a0e0d57e8faa5b9fe04ec1601a04d80be
Dana <Dana.ackerman0914@gmail.com>
2024-07-04 20:23:56 +0500

    Created sessionfactory and transaction

--- src/main/java/model/impl/TicketDaoImpl.java
+++ src/main/java/model/impl/TicketDaoImpl.java
@@ -1,148 +1,83 @@
 package model.impl;
 
-import db.DatabaseConnection;
 import model.dao.TicketDao;
 import model.entities.Ticket;
-import model.entities.TicketType;
-import model.entities.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
-import java.sql.*;
+
+
 public class TicketDaoImpl implements TicketDao {
-    static DatabaseConnection db;
-    static Connection con;
-
-    static {
-        try {
-            db = new DatabaseConnection();
-            con = db.getConnection();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+    SessionFactory sessionFactory;
+    public TicketDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
     @Override
-    public Ticket getTicketById(long id) throws SQLException {
-        con.setAutoCommit(false);
-        ResultSet rs;
-        String idQuery = "SELECT id, user_id, ticket_type, creation_date from ticket WHERE id=?";
-        try (PreparedStatement pr = con.prepareStatement(idQuery)) {
-            Savepoint savepoint1 = con.setSavepoint();
-            pr.setLong(1, id);
-            rs = pr.executeQuery();
-            if (rs.next()) {
-                return instantiateTicket(rs, new User());
-            }
-            con.rollback(savepoint1);
-            con.commit();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            con.setAutoCommit(true);
+    public void findById(long id) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            Ticket ticket = session.get(Ticket.class, id);
+            System.out.println(ticket);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return null;
-    }
-
-    @Override
-    public Ticket getTicketByUserId(long userId) throws SQLException{
-        con.setAutoCommit(false);
-        ResultSet rs;
-        String fetch = "SELECT id, user_id, ticket_type, creation_date from ticket WHERE user_id=?";
-        try (PreparedStatement pr = con.prepareStatement(fetch)) {
-            Savepoint savepoint1 = con.setSavepoint();
-            pr.setLong(1, userId);
-            rs = pr.executeQuery();
-            if (rs.next()) {
-                return instantiateTicket(rs, new User());
-            }
-            con.rollback(savepoint1);
-            con.commit();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            con.setAutoCommit(true);
-        }
-        return null;
 
     }
 
     @Override
-    public void save(Ticket ticket) throws SQLException {
-        con.setAutoCommit(false);
-        int rowsInserted;
-        String insert = "INSERT INTO ticket(user_id, ticket_type, creation_date) VALUES(?, CAST(? AS ticket_type), ?)";
-        try (PreparedStatement statement = con.prepareStatement(insert)) {
-            Savepoint savepoint1 = con.setSavepoint();
-            statement.setLong(1, ticket.getUserId());
-            statement.setString(2, ticket.getTicketType().name());
-            statement.setDate(3, Date.valueOf(ticket.getCreationDate()));
-            rowsInserted = statement.executeUpdate();
-            if (rowsInserted > 0) {
-                System.out.println("A new user was inserted successfully!");
-            }
-            con.rollback(savepoint1);
-            con.commit();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            con.setAutoCommit(true);
+    public void findByUserId(long userId) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            Ticket ticket = session.get(Ticket.class, userId);
+            System.out.println(ticket);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void save(Ticket ticket) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.createNativeQuery("INSERT INTO Ticket (user_id,ticket_type, creation_date) VALUES(?1, CAST(?2 AS ticket_type), ?3)", Ticket.class)
+                            .setParameter(1, ticket.getUserId())
+                            .setParameter(2, ticket.getTicketType())
+                            .setParameter(3, ticket.getCreationDate())
+                            .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     @Override
-    public void update(Ticket ticket) throws SQLException {
-        con.setAutoCommit(false);
-        int rowsUpdated;
-        String update = "UPDATE ticket SET ticket_type=CAST(? AS ticket_type) WHERE id=?";
-        try (PreparedStatement statement = con.prepareStatement(update)) {
-            Savepoint savepoint1 = con.setSavepoint();
-            statement.setString(1, ticket.getTicketType().name());
-            statement.setLong(2, ticket.getId());
-            rowsUpdated = statement.executeUpdate();
-            if (rowsUpdated > 0) {
-                System.out.println("An existing user was updated successfully!");
-            }
-            con.rollback(savepoint1);
-            con.commit();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            con.setAutoCommit(true);
+    public void update(Ticket ticket) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.createNativeQuery("UPDATE ticket SET ticket_type = CAST(?1 AS ticket_type) WHERE id = ?2")
+                    .setParameter(1, ticket.getTicketType())
+                    .setParameter(2, ticket.getId()).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 
     @Override
-    public void delete(long id) throws SQLException {
-        con.setAutoCommit(false);
-        int rowsDeleted;
-        String remove = "DELETE FROM ticket WHERE id=?";
-        try (PreparedStatement statement = con.prepareStatement(remove)) {
-            Savepoint savepoint1 = con.setSavepoint();
-            statement.setLong(1, id);
-            rowsDeleted = statement.executeUpdate();
-            if (rowsDeleted > 0) {
-                System.out.println("An existing user was deleted successfully!");
-            }
-            con.rollback(savepoint1);
-            con.commit();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            con.setAutoCommit(true);
+    public void delete(long id) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.createQuery("delete from Ticket t where t.id = :id")
+                    .setParameter("id", id).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
 
-    private Ticket instantiateTicket(ResultSet rs, User user) {
-        Ticket ticket = new Ticket();
-        try {
-            ticket.setId(rs.getLong("id"));
-            ticket.setTicketType(TicketType.valueOf(rs.getString("ticket_type")));
-            ticket.setUserId(rs.getLong("user_id"));
-            ticket.setUser(user);
-            ticket.setCreationDate(rs.getDate(("creation_date")).toLocalDate());
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return ticket;
     }
-
 }
--- src/main/java/model/impl/UserDaoImpl.java
+++ src/main/java/model/impl/UserDaoImpl.java
@@ -1,126 +1,68 @@
 package model.impl;
 
-import db.DatabaseConnection;
-import model.dao.UserDao;
-import model.entities.Ticket;
-import model.entities.User;
 
-import java.sql.*;
+import model.dao.UserDao;
+import model.entities.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 
 public class UserDaoImpl implements UserDao {
-    static DatabaseConnection db;
-    static Connection con;
+    SessionFactory sessionFactory;
 
-    static {
-        try {
-            db = new DatabaseConnection();
-            con = db.getConnection();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+    @Override
+    public void findById(long id) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            System.out.println(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     @Override
-    public User getUserById(long id) throws SQLException {
-        con.setAutoCommit(false);
-        ResultSet rs;
-        String fetch = "SELECT id, username, creation_date from users WHERE id=?";
-        try (PreparedStatement pr = con.prepareStatement(fetch)) {
-            Savepoint savepoint = con.setSavepoint("savepoint");
-            pr.setLong(1, id);
-            rs = pr.executeQuery();
-            if (rs.next()) {
-                return instantiateUser(rs, new Ticket());
-            }
-            con.rollback(savepoint);
-            con.commit();
-        }
-        catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            con.setAutoCommit(true);
-        }
-        return null;
-    }
-
-
-    @Override
-    public void save(User user) throws SQLException {
-        con.setAutoCommit(false);
-        int rowsInserted;
-        String insert = "INSERT INTO users(id, username, creation_date) VALUES(?, ?, ?)";
-        try (PreparedStatement statement = con.prepareStatement(insert)) {
-            Savepoint savepoint = con.setSavepoint("savepoint");
-            statement.setLong(1, user.getId());
-            statement.setString(2, user.getUsername());
-            statement.setDate(3, Date.valueOf(user.getCreationDate()));
-            rowsInserted = statement.executeUpdate();
-            if (rowsInserted > 0) {
-                System.out.println("A new user was inserted successfully!");
-            }
-            con.rollback(savepoint);
-            con.commit();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            con.setAutoCommit(true);
+    public void save(User user) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.createNativeQuery("INSERT INTO users (username, creation_date) VALUES (?1, ?2)", User.class)
+                    .setParameter(1, user.getUsername())
+                    .setParameter(2, user.getCreationDate()).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     @Override
-    public void update(User user) throws SQLException {
-        con.setAutoCommit(false);
-        int rowsUpdated;
-        String update = "UPDATE users SET username=? WHERE id=?";
-        try (PreparedStatement statement = con.prepareStatement(update)) {
-            Savepoint savepoint = con.setSavepoint("savepoint");
-            statement.setString(1, user.getUsername());
-            statement.setLong(2, user.getId());
-            rowsUpdated = statement.executeUpdate();
-            if (rowsUpdated > 0) {
-                System.out.println("An existing user was updated successfully!");
-            }
-            con.rollback(savepoint);
-            con.commit();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            con.setAutoCommit(true);
+    public void update(User user, long id) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.createNativeQuery("UPDATE users SET username = ?1, creation_date = ?2 WHERE id = ?3")
+                    .setParameter(1, user.getUsername())
+                    .setParameter(2, user.getCreationDate())
+                    .setParameter(3, id).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 
     @Override
-    public void delete(long id) throws SQLException {
-        con.setAutoCommit(false);
-        int rowsDeleted;
-        String delete = "DELETE FROM users WHERE id=?";
-        try (PreparedStatement statement = con.prepareStatement(delete)) {
-            Savepoint savepoint = con.setSavepoint("savepoint");
-            statement.setLong(1, id);
-            rowsDeleted = statement.executeUpdate();
-            if (rowsDeleted > 0) {
-                System.out.println("An existing user was deleted successfully!");
-            }
-            con.rollback(savepoint);
-            con.commit();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            con.setAutoCommit(true);
+    public void delete(long id) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.createQuery("DELETE FROM User u WHERE u.id = :id")
+                    .setParameter("id", id).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-
-    private User instantiateUser(ResultSet rs, Ticket ticket)  {
-        User user = new User();
-        try {
-            user.setId(rs.getLong("id"));
-            user.setUsername(rs.getString("username"));
-            user.setCreationDate(rs.getDate(("creation_date")).toLocalDate());
-            user.setTicket(ticket);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return user;
     }
 }
