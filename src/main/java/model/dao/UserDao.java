92accbb449965cfbf0ab1198bd905ba73a1b1601
Dana <Dana.ackerman0914@gmail.com>
2024-07-05 21:48:30 +0500

    Made changes in methods

--- src/main/java/model/dao/UserDao.java
+++ src/main/java/model/dao/UserDao.java
@@ -5,7 +5,7 @@ import model.entities.User;
 
 
 public interface UserDao {
-    void findById(long id);
+    void findUserById(long id);
     void save(User user);
     void update(User user, long id);
     void delete(long id);
